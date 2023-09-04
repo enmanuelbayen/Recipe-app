9b9e7326287c2c5a9c1d5f2a350e20c00f5324bd
enmanuelbayen <enmanuel0495@gmail.com>
2023-09-04 17:19:28 -0400

    add validations for users and foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 250, too_long: '%<count>s characters is the maximum allowed' }
+  validates :measurement_unit, presence: true, length: { maximum:50, '%<count>s characters is the maximum allowed' }
+  validates :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+has_many :foods
+has_many :recipes
+
+validates :name, presence: true, length: { maximum: 250, too_long: '%<count>s characters is the maximum allowed' }
 end
