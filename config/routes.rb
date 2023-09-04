7e64f6e2ff92d8d5062209f89d5a5ae26ed26f09
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-04 18:39:24 -0400

    Add tailwind config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     resources :foods
     resources :recipes
-    resources :public_recipes, only [:index]
-    resources :general_shopping_list, only [:index]
+    resources :public_recipes, only: [:index]
+    resources :general_shopping_list, only: [:index]
 end
