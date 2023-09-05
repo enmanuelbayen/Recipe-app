39ec59d276a10b22645cf5916a5b005f15ad815f
enmanuelbayen <enmanuel0495@gmail.com>
2023-09-05 16:00:45 -0400

    Update methods in foods/recipe controllers and update views for boths

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,32 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = Recipe.all
+  end
 
   def show; end
 
-  def create; end
+  def create
+    @recipe = Recipe.new
 
-  def new; end
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
 
-  def destroy; end
+  def new
+    @recipe = Recipe.new
+  end
+
+  def destroy
+    @recipe.destroy
+
+    respond_to do |format|
+      format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    resources :foods
+    resources :foods, only: [:index, :create, :new, :destroy]
     resources :recipes
     resources :public_recipes, only: [:index]
     resources :general_shopping_list, only: [:index]
