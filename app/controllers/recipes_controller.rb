cf37193b89b44181af8fd4ddedc235d5b6f10178
enmanuelbayen <enmanuel0495@gmail.com>
2023-09-05 11:42:53 -0400

    linter fix

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,11 @@
 class RecipesController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def create
-  end
+  def create; end
 
-  def new
-  end
+  def new; end
 
-  def destroy
-  end
+  def destroy; end
 end
