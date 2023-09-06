432f7ebd757fef48a7021cbc14bc3a3aca05f6ac
Luiscarlosvd <lc26cr7@gmail.com>
2023-09-06 13:56:39 -0400

    Add basic view for general shopping list

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,3 +1,8 @@
 class GeneralShoppingListController < ApplicationController
-  def index; end
+  def index
+    @user = current_user
+    @recipes = current_user.recipes.joins(:recipe_foods).select('recipe_foods.*')
+
+
+  end
 end
--- app/helpers/general_shopping_list_helper.rb
+++ app/helpers/general_shopping_list_helper.rb
@@ -1,2 +1,22 @@
 module GeneralShoppingListHelper
+    def generate_missing_foods(user, recipe_foods)
+        user_foods = user.foods
+        user_recipe_foods = recipe_foods.group_by(&:food_id).transform_values { |foods| foods.sum(&:quantity) }
+
+        p user_recipe_foods
+    
+        missing_foods = []
+    
+        user_recipe_foods.each do |food_id, recipe_food_quantity|
+            user_food = user_foods.find_by(id: food_id)
+            if user_food && user_food.quantity < recipe_food_quantity
+              missing_foods << {
+                food: user_food,
+                quantity_remaining: recipe_food_quantity - user_food.quantity
+              }
+            end
+        end
+    
+        missing_foods
+    end
 end
