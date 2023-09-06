3f39132ca29091608661a68d5dd1d8acdf4cd926
enmanuelbayen <enmanuel0495@gmail.com>
2023-09-06 19:27:25 -0400

    unit test for recipe and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 250, too_long: '%<count>s characters is the maximum allowed' }
+  validates :name, presence: true, length: { maximum: 250, message: '%<count>s characters is the maximum allowed' }
 end
